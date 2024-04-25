c59258776e92fa811b9d7297c3cdd56aea5d705d
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-25 15:46:13 +0500

    github

---
+++ Q35.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// creating an array
+let petAnimals = ["Rabbit", "Dog", "Cat"];
+// Using for loop
+for (let onePetName of petAnimals) {
+    console.log(`A ${onePetName} would make a great pet`);
+}
+// print a message aouside the loop
+console.log("Any of these animals would make a great pet!!");
---
+++ Q35.ts
@@ -0,0 +1,8 @@
+// creating an array
+let petAnimals = ["Rabbit","Dog","Cat"]
+// Using for loop
+for(let onePetName of  petAnimals){
+    console.log(`A ${onePetName} would make a great pet`);
+}
+// print a message aouside the loop
+console.log("Any of these animals would make a great pet!!");
